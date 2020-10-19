0278be4fc3e5f392d98cd49db4a4931705c58c82
adekeagle <2726949-adekeagle@users.noreply.gitlab.com>
2020-10-20 00:12:31 +0200

    Initial commit

---
+++ src/test/java/pl/adcom/restapiready/RestApiReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adcom.restapiready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
